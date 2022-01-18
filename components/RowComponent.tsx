ea6165161ee42cef5d87b9eeccdbb22e0cd51a4a
Christophe Prat <christopheprat@icloud.com>
2022-01-18 19:57:03 +0100

    Change graphical identity

--- components/RowComponent.tsx
+++ components/RowComponent.tsx
@@ -20,11 +20,11 @@ const RowComponent = (data: { id: any, title: string, extract: string, videoLink
 		return (
 			<>
 				<div className="flex-col flex-auto m-1 overflow-hidden text-white rounded-lg shadow-lg md:w-1/2 md:m-5 shrink-0 p-x-1 md:p-x-4 h-fit">
-					<div className="flex justify-between w-full p-2 text-lg font-bold bg-blue-500">
+					<div className="flex items-center justify-between w-full p-2 text-lg font-bold bg-blue-500">
 						<h2>{data.title}</h2>
 						<div>
 							<a href={data.videoLink} target="_blank" rel="noreferrer">
-								<svg xmlns="http://www.w3.org/2000/svg" className="inline-block w-6 h-6 m-2 align-middle" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+								<svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 m-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 									<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
 								</svg>
 							</a>
